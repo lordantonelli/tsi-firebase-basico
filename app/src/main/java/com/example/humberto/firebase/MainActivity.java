46f0b39707d5133177c0447ac6d8c2d7963df6dc
Humberto Lidio Antonelli <humbertoantonelli@usp.br>
2022-10-28 17:32:18 -0400

    Projeto base inicial

--- app/src/main/java/com/example/humberto/firebase/MainActivity.java
+++ app/src/main/java/com/example/humberto/firebase/MainActivity.java
@@ -16,9 +16,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    // Atributo: referência para o nosso banco de dados!
-    // Esta referência "aponta" para o nó RAIZ da árvore!
-    private DatabaseReference BD = FirebaseDatabase.getInstance().getReference();
+
     // Atributos que representam os objetos da interface gráfica:
     private EditText txtNome;
     private EditText txtSobrenome;
@@ -40,54 +38,7 @@ public class MainActivity extends AppCompatActivity {
         lblNome = findViewById(R.id.lblNome);
         lblSobrenome = findViewById(R.id.lblSobrenome);
         lblIdade = findViewById(R.id.lblIdade);
-
-        btnEnviar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Variáveis para os dados digitados nas caixas
-                String nome, sobren;
-                int idade;
-                // Pegando os dados nas caixas.
-                // ATENÇÃO!! não vamos fazer filtros de erros!
-                nome = txtNome.getText().toString();
-                sobren = txtSobrenome.getText().toString();
-                idade = Integer.parseInt( txtIdade.getText().toString());
-                // Criando o objeto Usuario, com os dados acima
-                Usuario u = new Usuario(nome, sobren, idade);
-                // Vamos ter apenas um nó, "dados", então podemos gravar direto
-                BD.child("dados").setValue(u);
-            }
-        });
-
-        // Criando uma referência para o nó "dados" no Firebase:
-        DatabaseReference dados = BD.child("dados");
-        // Criando e associando um escutador neste nó:
-        dados.addValueEventListener(new EscutadorFirebase());
     }
 
-    // Classe interna no escutador do Firebase
-    private class EscutadorFirebase implements ValueEventListener {
-        // O método onDataChange é chamado em 2 ocasiões:
-        // - assim que o Escutador é criado e associado ao respectivo nó;
-        // - sempre que houver alguma alteração nos dados.
-        @Override
-        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-            // Os dados recuperados no Firebase vem dentro de um DataSnapshot.
-            // No nosso caso, só vai ter um objeto lá dentro (objeto da classe Usuario).
-            // Precisamos testar se veio alguma informação, isto é, se o dataSnapshot existe...
-            if ( dataSnapshot.exists()) {
-                // Recuperamos o objeto Usuario que veio dentro do dataSnapshot:
-                Usuario u = dataSnapshot.getValue(Usuario.class);
-                // Colocando os dados do objeto lido na interface gráfica.
-                lblNome.setText(u.getNome());
-                lblSobrenome.setText(u.getSobrenome());
-                lblIdade.setText(Integer.toString(u.getIdade()));
-            }
-        }
-
-        // Não trabalharemos com este método. Mas ele tem que existir!!
-        @Override
-        public void onCancelled(@NonNull DatabaseError databaseError) { }
-    }
 }
 
